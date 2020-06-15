f5ed89b3cfc39591a6c8903c8c74120685e292d1
CityChan <huanchengch@gmail.com>
2020-06-15 11:36:18 -0500

    Site updated: 2020-06-15 11:36:16

--- dist/music.js
+++ dist/music.js
@@ -1,14 +1,13 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
-    autoplay: false,
+    autoplay: true,
     audio: [
       {
         name: "stairway to heaven",
         artist: 'Led Zeppelin',
         url: 'http://antiserver.kuwo.cn/anti.s?useless=/resource/&format=mp3&rid=MUSIC_83826476&response=res&type=convert_url&',
         cover: '',
-        autoplay: true,
       }
     ]
 });
